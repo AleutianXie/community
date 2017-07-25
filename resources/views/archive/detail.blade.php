471daede5ce9d32a854a68d8826a2c0352498824
xiehuisheng <xiehuisheng@yongche.com>
2017-07-25 22:34:56 +0800

    update silder

--- resources/views/archive/detail.blade.php
+++ resources/views/archive/detail.blade.php
@@ -96,7 +96,7 @@
                                     <div id="jssor_1" style="margin:0 auto;top:0px;left:0px;width:980px;height:580px;overflow:hidden;visibility:hidden;">
                                         <!-- Loading Screen -->
                                         <div data-u="loading" class="jssorl-004-double-tail-spin" style="position:absolute;top:0px;left:0px;text-align:center;background-color:rgba(0,0,0,0.7);">
-                                            <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="img/double-tail-spin.svg" />
+                                            <img style="margin-top:-19px;position:relative;top:50%;width:38px;height:38px;" src="{{ asset('img/double-tail-spin.svg') }}" />
                                         </div>
                                         <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:580px;overflow:hidden;">
                                             <div>
