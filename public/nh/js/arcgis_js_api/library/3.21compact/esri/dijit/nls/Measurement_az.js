17b30df36c0e0a3721302aebedc2209bed94a1e9
XHS84-PC\huis_ <aleutian.xie@cicisoft.cn>
2017-09-06 03:10:28 +0800

    delete nh demo

--- public/nh/js/arcgis_js_api/library/3.21compact/esri/dijit/nls/Measurement_az.js
+++
@@ -1,4 +0,0 @@
-// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
-// See http://js.arcgis.com/3.21/esri/copyright.txt for details.
-//>>built
-define("esri/dijit/nls/Measurement_az",{"dijit/nls/loading":{loadingState:"Y\u00fckl\u0259nir...",errorState:"Problem yarand\u0131",_localized:{}}});
--- public/nh/js/arcgis_js_api/library/3.21compact/esri/dijit/nls/Measurement_nl-nl.js
+++
@@ -1,4 +0,0 @@
-// All material copyright ESRI, All Rights Reserved, unless otherwise specified.
-// See http://js.arcgis.com/3.21/esri/copyright.txt for details.
-//>>built
-define("esri/dijit/nls/Measurement_nl-nl",{"dijit/nls/loading":{loadingState:"Bezig met laden...",errorState:"Er is een fout opgetreden",_localized:{}}});
